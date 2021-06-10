4da0da58e349f3d993a7812fc0d53e74498eff1c
Shkunn <leclercqted@gmail.com>
2021-06-11 00:29:21 +0200

    [ADD] websockify

--- launch_server.sh
+++ launch_server.sh
@@ -4,4 +4,4 @@ gnome-terminal --command="bash -c 'python3 SERVER/server.py; $SHELL'"
 
 gnome-terminal --command="bash -c 'python3 SERVER/app_core.py --prototxt MobileNetSSD_deploy.prototxt --montageW 2 --montageH 2; $SHELL'"
 
-gnome-terminal --command="bash -c './../websockify-js/websockify/websockify.js localhost:8081 172.21.72.168:5900; $SHELL'"
+gnome-terminal --command="bash -c './ws/websockify/websockify.js localhost:8081 172.21.72.168:5900; $SHELL'"
